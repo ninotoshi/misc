313159ff945d42942256bf4d2bd48d15cf7ade2c
ninotoshi <ninotoshi@users.noreply.github.com>
2014-11-07 23:27:54 +0900

    added a brew script

---
+++ brew/setup.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+help() {
+  cat << EOF
+$0 -- install or upgrade formulae
+Usage: $0 [-hic] [formula ...]
+-h: display this message
+-i: only install, do not upgrade
+-c: run 'brew cask' instead of 'brew'
+Note: -c implicitly activates -i
+EOF
+}
+
+upgrade_existing() {
+  echo $CMD upgrade $1
+}
+
+echo_existing() {
+  echo $1 is already installed
+}
+
+init() {
+  export HOMEBREW_CASK_OPTS="$HOMEBREW_CASK_OPTS"
+  CMD="brew"
+  EXISTING="upgrade_existing"
+
+  OPTIND=1
+  while getopts "hic" opt; do
+      case "$opt" in
+          h)
+              help
+              exit 0
+              ;;
+          i)
+              EXISTING="echo_existing"
+              ;;
+          c)
+              EXISTING="echo_existing"
+              CMD="brew cask"
+              ;;
+          '?')
+              help > /dev/stderr
+              exit 1
+              ;;
+      esac
+  done
+
+  shift $((OPTIND-1))
+
+  APPS="$@"
+}
+
+run() {
+  init "$@"
+  echo params are $@
+  echo APPS is $APPS
+  echo CMD is $CMD
+  for app in $APPS; do
+    if $CMD list $app > /dev/null 2>&1; then
+      $EXISTING $app
+    else
+      echo $CMD install $app
+    fi
+  done
+}
+
+run "$@"
